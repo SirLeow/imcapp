bd5765acbc035493b2fe25b61f9950a73eb87769
Thiago Barros <t.b.lima18@gmail.com>
2022-03-01 17:24:07 -0300

    Added the calculation to the button

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        calcButton.setOnClickListener{
+            val pesoTemp = peso.text.toString().toDouble()
+            val alturaTemp = altura.text.toString().toDouble()
+            val imc = (pesoTemp/(alturaTemp*alturaTemp))
+            val imcTemp = "%.2f".format(imc)
+            var imcText = ""
+
+            when{
+                imc < 18.5  -> imcText = "Magreza"
+                imc <= 24.9 -> imcText = "Normal"
+                imc <= 29.9 -> imcText = "Sobrepeso"
+                imc <= 34.9 -> imcText = "Obesidade grau 1"
+                imc <= 39.9 -> imcText = "Obesidade grau 2"
+                imc >= 40   -> imcText = "Obesidade Morbida"
+            }
+            imcText = "$imcTemp - $imcText"
+            Result.text = imcText
+        }
     }
 }
